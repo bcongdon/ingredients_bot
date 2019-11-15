32071a785ebbd68d76d2f96a7bdb6b2eb562f577
Benjamin Congdon <bencongdon96@gmail.com>
2019-11-14 18:55:07 -0800

    Add formatting code for Food -> Tweets

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,41 @@ struct Food {
     ingredients: Vec<String>,
 }
 
+const UNBRANDED_OWNER: &str = "Not a Branded Item";
+
+impl Food {
+    fn header(&self) -> String {
+        if self.brand_owner == UNBRANDED_OWNER.to_owned() {
+            return format!("\"{}\"", self.description);
+        }
+        format!("\"{}\" (by {})", self.description, self.brand_owner)
+    }
+
+    fn to_tweets(&self) -> Vec<String> {
+        self.ingredients.iter().enumerate().fold(
+            vec![self.header() + "\n\nIngredients: "],
+            |mut acc, (idx, x)| {
+                let last = acc.last_mut().unwrap();
+                if last.len() + x.len() > 240 {
+                    last.truncate(last.len() - 1);
+                    acc.push(x.to_string() + ",")
+                } else {
+                    if last.ends_with(',') {
+                        last.push_str(" ");
+                    }
+                    last.push_str(x);
+                    if idx != self.ingredients.len() - 1 {
+                        last.push_str(",");
+                    }
+                }
+                acc
+            },
+        )
+    }
+}
+
 fn title_case(s: String) -> String {
-    titlecase::titlecase(&s).to_owned()
+    titlecase::titlecase(&s)
 }
 
 fn main() -> Result<()> {
@@ -32,14 +65,14 @@ fn main() -> Result<()> {
             brand_owner: title_case(row.get(0)?),
             description: title_case(row.get(1)?),
             ingredients: ingredients_raw
-                .split(",")
+                .split(',')
                 .map(|s| s.trim().to_string())
                 .map(title_case)
                 .collect::<Vec<String>>(),
         })
     });
 
-    println!("{:?}", food.unwrap());
+    println!("{}", food.unwrap().to_tweets().join("\n---\n"));
 
     Ok(())
 }
