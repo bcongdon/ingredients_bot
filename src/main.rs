1076532752ba34e876cbfcc317e4bc06c5c93570
Benjamin Congdon <bencongdon96@gmail.com>
2019-11-14 20:14:51 -0800

    Add ability to tweet

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+extern crate egg_mode;
 extern crate rusqlite;
 extern crate titlecase;
+extern crate tokio;
 
+use rusqlite::Connection;
 use rusqlite::NO_PARAMS;
-use rusqlite::{Connection, Result};
+use tokio::runtime::current_thread::block_on_all;
 
 #[derive(Debug)]
 struct Food {
@@ -44,11 +47,34 @@ impl Food {
     }
 }
 
+struct Config {
+    pub token: egg_mode::Token,
+}
+
+impl Config {
+    fn load() -> Self {
+        let consumer_key = std::env::var("CONSUMER_KEY").unwrap().trim().to_string();
+        let consumer_secret = std::env::var("CONSUMER_SECRET").unwrap().trim().to_string();
+        let con_token = egg_mode::KeyPair::new(consumer_key, consumer_secret);
+
+        let access_token = std::env::var("ACCESS_TOKEN").unwrap().trim().to_string();
+        let access_secret = std::env::var("ACCESS_SECRET").unwrap().trim().to_string();
+        let acc_token = egg_mode::KeyPair::new(access_token, access_secret);
+
+        Config {
+            token: egg_mode::Token::Access {
+                consumer: con_token,
+                access: acc_token,
+            },
+        }
+    }
+}
+
 fn title_case(s: String) -> String {
     titlecase::titlecase(&s)
 }
 
-fn main() -> Result<()> {
+fn main() -> rusqlite::Result<()> {
     let conn = Connection::open("food.db")?;
 
     let mut stmt = conn.prepare(
@@ -59,20 +85,34 @@ fn main() -> Result<()> {
         LIMIT 11;",
     )?;
 
-    let food = stmt.query_row(NO_PARAMS, |row| {
-        let ingredients_raw: String = row.get(2)?;
-        Ok(Food {
-            brand_owner: title_case(row.get(0)?),
-            description: title_case(row.get(1)?),
-            ingredients: ingredients_raw
-                .split(',')
-                .map(|s| s.trim().to_string())
-                .map(title_case)
-                .collect::<Vec<String>>(),
+    let food = stmt
+        .query_row(NO_PARAMS, |row| {
+            let ingredients_raw: String = row.get(2)?;
+            Ok(Food {
+                brand_owner: title_case(row.get(0)?),
+                description: title_case(row.get(1)?),
+                ingredients: ingredients_raw
+                    .split(',')
+                    .map(|s| s.trim().to_string())
+                    .map(title_case)
+                    .collect::<Vec<String>>(),
+            })
         })
-    });
+        .unwrap();
 
-    println!("{}", food.unwrap().to_tweets().join("\n---\n"));
+    println!("{}", food.to_tweets().join("\n---\n"));
+
+    let config = Config::load();
+
+    let mut prev_tweet_id: Option<u64> = None;
+    for tweet in food.to_tweets().iter() {
+        let mut draft = egg_mode::tweet::DraftTweet::new(tweet);
+        if let Some(tweet_id) = prev_tweet_id {
+            draft = draft.in_reply_to(tweet_id);
+        }
+        let tweet = block_on_all(draft.send(&config.token)).unwrap();
+        prev_tweet_id = Some(tweet.id);
+    }
 
     Ok(())
 }
